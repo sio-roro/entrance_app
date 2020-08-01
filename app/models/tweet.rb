f80c90d49412faadbfec233c6689a992502310ad
sio-maker <sio0040maker@gmai.com>
2020-08-01 19:42:19 +0900

    Release to heroku

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :body, presence: true,    length: { maximum: 50 }    
+  validates :body, presence: true,    length: { maximum: 140 }    
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
