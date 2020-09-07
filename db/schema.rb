08db922902e8948bc43e3102e1dc24376457da1b
sio-maker <sio0040maker@gmai.com>
2020-09-07 16:55:22 +0900

    add admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_051215) do
+ActiveRecord::Schema.define(version: 2020_09_07_072848) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comment"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_051215) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username", default: "", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
